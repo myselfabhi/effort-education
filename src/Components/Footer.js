f8bb886f468efe81e783c92bf87abd35f7b021cb
myselfabhi <abhinavverma2024@gmail.com>
2024-03-07 00:53:24 +0530

    Header and footer done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
-      <p>&copy; {new Date().getFullYear()} Effort Education. All rights reserved.</p>
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; {new Date().getFullYear()} Effort Education. All rights reserved.</p>
+      </div>
     </footer>
   );
 }
 
 export default Footer;
+
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,15 +3,36 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/courses">Courses</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
+    <header className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Left-aligned components (Navigation links) */}
+        <nav className="flex">
+          <ul className="flex">
+            <li className="mr-6">
+              <Link to="/" className="hover:text-gray-300">Home</Link>
+            </li>
+            <li className="mr-6">
+              <Link to="/courses" className="hover:text-gray-300">Courses</Link>
+            </li>
+            <li className="mr-6">
+              <Link to="/about" className="hover:text-gray-300">About</Link>
+            </li>
+            <li>
+              <Link to="/contact" className="hover:text-gray-300">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Center-aligned logo (Site title) */}
+        <div className="text-2xl font-bold">
+          Effort Education
+        </div>
+
+        {/* Right-aligned components (User profile, etc.) */}
+        <div>
+          {/* Add your logo or any other component here */}
+        </div>
+      </div>
     </header>
   );
 }
